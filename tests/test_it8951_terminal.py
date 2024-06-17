5b0a64d67346f72fb56968e10563862ee78a8393
filip-we <filip.wettlen@gmail.com>
2024-06-17 22:10:48 +0200

    Working test of showtext function

--- tests/test_it8951_terminal.py
+++ tests/test_it8951_terminal.py
@@ -1,22 +1,26 @@
+import pytest
+
 from papertty.papertty import PaperTTY, Settings
 
+# Variables that you might have to tweak to get the test working in your environment and your screen model.
+TRUE_TYPE_FONT = "/usr/share/fonts/truetype/dejavu/DejaVuSansMono.ttf"
+VCOM = 1780
 
-def test_test():
-    terminal_nr = 15
+@pytest.fixture
+def settings():
     settings = Settings(driver="IT8951", partial=True, encoding="latin_1")
-    settings.args['vcom'] = 1780
-    settings.args['font'] = "/".join(PaperTTY.defaultfont.split("/")[:-2]) + "/tom-thumb.pbm"
-    settings.args['fontsize'] = 80
+    settings.args['vcom'] = VCOM
+    settings.args['font'] = TRUE_TYPE_FONT
+    settings.args['fontsize'] = 40
+    return settings
+
+def test_showtext(settings):
     ptty = settings.get_init_tty()
 
-    ptty.set_tty_size(f"/dev/tty{terminal_nr}", 20, 80)
-
-    max_dim = ptty.fit(portrait=True)
+    ptty.set_tty_size("/dev/tty15", 20, 80)
 
     ptty.font = ptty.load_font(settings.args["font"], keep_if_not_found=True)
-    ptty.showtext("Hello!", fill=ptty.white, portrait=True)
     ptty.showtext("Hello!\nWorld.", fill=ptty.black, portrait=True)
-    assert 1 == 2
 
 # TODO: print warning (somewhere) if no font has been defined. If it is not defined, no text will be printed.
 
